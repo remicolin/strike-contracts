9aaa299d01351275d9271210135bdd930e21f7de
remi colin <colin.remi07@gmail.com>
2023-02-21 16:13:58 +0100

    adding Strike controller

---
+++ src/StrikeController.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+import "./StrikePool.sol";
+import "./interfaces/IStrikePool.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+pragma solidity ^0.8.13;
+
+contract StrikeController is Ownable {
+    address public erc20;
+    mapping(address => address) public pools;
+    address[] public erc721;
+    event PoolDeployed(address erc721, address erc20, address pool);
+    address auctionManager;
+    address optionPricing;
+    address volatilityOracle;
+
+    constructor(address _erc20) {
+        erc20 = _erc20;
+    }
+
+    function deployPool(
+        address _erc721
+    ) public onlyOwner returns (address pool) {
+        require(pools[_erc721] == address(0));
+        StrikePool jpegxPool = new StrikePool(_erc721, erc20, auctionManager);
+        pools[_erc721] = address(jpegxPool);
+        erc721.push(_erc721);
+        jpegxPool.transferOwnership(owner());
+        emit PoolDeployed(_erc721, erc20, address(jpegxPool));
+        return address(jpegxPool);
+    }
+
+    function erasePool(address _erc721) public onlyOwner {
+        pools[_erc721] = address(0);
+    }
+
+    function getPoolFromTokenAddress(
+        address _tokenAddress
+    ) public view returns (address) {
+        return pools[_tokenAddress];
+    }
+
+    function setAuctionManager(address _auctionManager) public onlyOwner {
+        auctionManager = _auctionManager;
+    }
+
+    function getAuctionManager() public view returns (address) {
+        return auctionManager;
+    }
+
+    /***    Option Pricing relatives functions     ***/
+    function setOptionPricing(address _optionPricing) public onlyOwner {
+        optionPricing = _optionPricing;
+    }
+
+    function getOptionPricing() public view returns (address) {
+        return optionPricing;
+    }
+
+    function setVolatilityOracle(address _volatilityOracle) public onlyOwner {
+        volatilityOracle = _volatilityOracle;
+    }
+
+    function getVolatilityOracle() public view returns (address) {
+        return volatilityOracle;
+    }
+
+    /*** Call PoolContract - setters functions  ***/
+    function setPoolAuctionManager(
+        address _pool,
+        address _auctionManager
+    ) public onlyOwner {
+        IStrikePool(_pool).setAuctionManager(_auctionManager);
+    }
+
+    /*** Call PoolContract - getters functions  ***/
+
+    function getfloorprice(
+        address _pool,
+        uint256 _epoch
+    ) public view returns (uint256) {
+        return IStrikePool(_pool).getfloorprice(_epoch);
+    }
+
+    function getEpoch_2e(address _pool) public view returns (uint256) {
+        return IStrikePool(_pool).getEpoch_2e();
+    }
+
+    function getSharesAtOf(
+        address _pool,
+        uint256 _epoch,
+        uint256 _strikePrice,
+        address _add
+    ) public view returns (uint256) {
+        return IStrikePool(_pool).getSharesAtOf(_epoch, _strikePrice, _add);
+    }
+
+    function getAmountLockedAt(
+        address _pool,
+        uint256 _epoch,
+        uint256 _strikePrice
+    ) public view returns (uint256) {
+        return IStrikePool(_pool).getAmountLockedAt(_epoch, _strikePrice);
+    }
+
+    function getOptionAvailableAt(
+        address _pool,
+        uint256 _epoch,
+        uint256 _strikePrice
+    ) public view returns (uint256) {
+        return IStrikePool(_pool).getOptionAvailableAt(_epoch, _strikePrice);
+    }
+
+    function getEpochDuration(
+        address _pool
+    ) public view returns (uint256 epochduration) {
+        return IStrikePool(_pool).getEpochDuration();
+    }
+
+    function getInterval(address _pool) public view returns (uint256 interval) {
+        return IStrikePool(_pool).getInterval();
+    }
+}
